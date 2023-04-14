76aaf2c7f130fc68c8d2eb12836449a4ea0a6460
Veresk <ZLveresk@gmail.com>
2023-04-14 21:42:33 +0300

    v13

--- foodgram/api/urls.py
+++ foodgram/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from .views import (CustomUserViewSet, FavoriteMixin, FollowListMixin,
                     FollowMixin, IngredientMixin, RecipeWiewSet,
-                    ShoppingCartMixin, TagViewSet,)
+                    ShoppingCartMixin, TagViewSet)
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('tags', TagViewSet)
