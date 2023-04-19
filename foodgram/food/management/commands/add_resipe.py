eda28495d5d3e897bef2cc7e55af1deca9febb00
Veresk <ZLveresk@gmail.com>
2023-04-19 16:39:22 +0300

    4 rewi

--- foodgram/food/management/commands/add_resipe.py
+++ foodgram/food/management/commands/add_resipe.py
@@ -3,7 +3,6 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-
 from food.models import Ingredients
 
 DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
